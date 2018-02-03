9ea6191b14853024eabaaf0a28b09e49854f04a7
危乖娥 <35945131+Weiguaie@users.noreply.github.com>
2018-02-03 15:49:10 +0800

    ww

---
+++ js/Personal-Center.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	//图片最后一个DIV
+	$(".lui-person-icon:last-child").addClass("lui-i-last");  
+	$(".lui-person-icon-viewbox ul li:last-child").addClass("lui-iconview-last");
+
+});
